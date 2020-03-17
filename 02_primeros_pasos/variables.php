6b66dc9cca5800b11cc6e4f89681f3ad9f62959e
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-03-17 13:18:27 -0700

    Definir variables
    Se describió la forma correcta de definir variables

---
+++ 02_primeros_pasos/variables.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Variables
+ * 
+ * Deben iniciar con el símbolo $
+ * Puede contener letras (del alfabeto inglés), números y _
+ * Despues del $ debe iniciar con una letra o _
+ * No puede incluir espacios (utiliza _ en su lugar)
+ * PHP distingue entre mayúsculas y minúsculas (Case Sensitive)
+ * No se puede utilizar palabras reservadas
+ * Deben nombrarlas con sentido (en base a su utilización)
+ * El tipo de dato se asigna por inferencia igual que en C# (var nombre = "Bidkar";)
+ */
+
+// Ejemplos
+$nombre = "Bidkar";
+$apellidos = "Aragón";
+$nombreCompleto = "Bidkar Aragón Cárdenas";
+$nombre_completo = "Bidkar Aragón Cárdenas";
+$edad = 40;
+$estatura = 1.78;
+$peso = "96kg";
+$_hobby = "Basketball";
+$EsDocente = true;
+// $mi nombre = "Bidkar"; no debe llevar espacios
+// $1parcial = 10; debe iniciar con $ seguido de letra o _
